93f43e57491e6846e6176b99fd1fdcf3a6ceb84c
Jayaprakash Aluri <alurijayaprakash@gmail.com>
2022-07-20 15:49:21 +0530

    list of java objects program added

--- basicREST_java_object/src/main/java/com/springIntro/rest/StudentController.java
+++ basicREST_java_object/src/main/java/com/springIntro/rest/StudentController.java
@@ -1,5 +1,8 @@
 package com.springIntro.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +10,22 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class StudentController {
 	
-	@GetMapping("/studentData")
+	@GetMapping("/student")
 	
 	public Student getStudent() {
 		return new Student("Jaya", "Prakash");
+	}
+	
+	@GetMapping("/students")
+	
+	public List<Student> getStudents() {
+		
+		List<Student> students = new ArrayList<>();
+		students.add(new Student("Sony", "Company"));
+		students.add(new Student("Dell", "Company"));
+		students.add(new Student("Tech", "Mojo"));
+		students.add(new Student("MI", "India"));
+		return students;
 	}
 
 }
