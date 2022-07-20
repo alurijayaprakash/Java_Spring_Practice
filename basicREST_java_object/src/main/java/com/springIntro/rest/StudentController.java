0599784b70de20bbc713454adcf487a8b7759e7e
Jayaprakash Aluri <alurijayaprakash@gmail.com>
2022-07-20 16:21:33 +0530

    Path Variable program added

--- basicREST_java_object/src/main/java/com/springIntro/rest/StudentController.java
+++ basicREST_java_object/src/main/java/com/springIntro/rest/StudentController.java
@@ -4,17 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class StudentController {
 	
+	// get single java object
+	
 	@GetMapping("/student")
 	
 	public Student getStudent() {
 		return new Student("Jaya", "Prakash");
 	}
+	
+	
+	// get list of Java Objects
 	
 	@GetMapping("/students")
 	
@@ -26,6 +32,13 @@ public class StudentController {
 		students.add(new Student("Tech", "Mojo"));
 		students.add(new Student("MI", "India"));
 		return students;
+	}
+	
+	// use path variable to read data from end point
+	// @PathVariable Annotation 
+	@GetMapping("/student/{firstName}/{lastName}")
+	public Student getStudentPathVariable(@PathVariable ("firstName") String firstName, @PathVariable ("lastName") String lastName) {
+		return new Student(firstName, lastName);
 	}
 
 }
